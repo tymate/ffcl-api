9146a50673bab3edd31e591251b98e2020d09151
max.jos <48556853+yhru@users.noreply.github.com>
2022-11-25 11:46:08 +0100

    Update app/graphql/mutations/update_club.rb
    Co-authored-by: Christian Huang <41517143+chrhuang@users.noreply.github.com>

--- app/graphql/mutations/update_club.rb
+++ app/graphql/mutations/update_club.rb
@@ -4,14 +4,13 @@ module Mutations
   class UpdateClub < BaseMutation
     description 'Update a club'
 
-    argument :id, ID, required: true
+    argument :club_id, ID, required: true, loads: Types::ClubType
     argument :label, String, required: false
     argument :description, String, required: false
 
     field :club, Types::ClubType, null: true
 
-    def resolve(id:, **args)
-      club = Club.find(id)
+    def resolve(club:, **args)
       authorize! club, to: :update?, with: ClubPolicy
 
       club.update!(**args)
